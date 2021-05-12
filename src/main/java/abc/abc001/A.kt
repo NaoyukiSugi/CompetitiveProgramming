c66cb28b7e60e8ef30bef4114721966840876801
shishamo <bnsgny@gmail.com>
2021-05-12 14:27:25 +0900

    solve (#2)

---
+++ src/main/java/abc/abc001/A.kt
@@ -0,0 +1,11 @@
+package abc.abc001
+
+fun main() {
+    val h1 = nextInt()
+    val h2 = nextInt()
+    val result = h1 - h2
+    println(result)
+}
+
+fun next() = readLine()!!
+fun nextInt() = next().toInt()
