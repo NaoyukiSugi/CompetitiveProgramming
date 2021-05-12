b713b015c49707edd3111da7e93146bc649db770
Naoyuki Sugi <bnsgny@gmail.com>
2021-05-12 15:55:16 +0900

    update

--- src/main/java/abc/abc001/A.kt
+++ src/main/java/abc/abc001/A.kt
@@ -1,11 +1,10 @@
 package abc.abc001
 
+import nextInt
+
 fun main() {
     val h1 = nextInt()
     val h2 = nextInt()
     val result = h1 - h2
     println(result)
 }
-
-fun next() = readLine()!!
-fun nextInt() = next().toInt()
