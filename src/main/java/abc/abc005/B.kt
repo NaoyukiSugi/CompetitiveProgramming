edb2052810ac15154295cfa57a5283ff6c3af0e3
shishamo <bnsgny@gmail.com>
2021-05-23 21:53:18 +0900

    abc005-B & downgrade kotlin version to 1.3.71 (#16)
    * solve
    
    * fix
    
    * fix

---
+++ src/main/java/abc/abc005/B.kt
@@ -0,0 +1,13 @@
+package abc.abc005
+
+import nextInt
+
+fun main() {
+    val n = nextInt()
+    val tList = mutableListOf<Int>()
+    for (i in 0 until n) {
+        val t = nextInt()
+        tList.add(t)
+    }
+    println(tList.min())
+}
